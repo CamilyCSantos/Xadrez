b0ff120e00c5ccc06d2b3bbef14d880f3eaf7915
Camily Santos <camilycsantos@gmail.com>
2023-11-28 21:52:48 -0300

    Implementando a logica de xeque mate

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -44,9 +44,8 @@ namespace XadrezConsole
                         Console.ReadLine();
                     }
                 }
-
-
-                    Tela.imprimirTabuleiro(partida.tab);
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch (TabuleiroException e) 
             {
