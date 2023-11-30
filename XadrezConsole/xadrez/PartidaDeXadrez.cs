9d6ad4e43c61ad029815ff7436e5b5bbadff8c9e
Camily Santos <camilycsantos@gmail.com>
2023-11-29 21:10:44 -0300

    Jogada especial roque pequeno e roque grande

--- XadrezConsole/xadrez/PartidaDeXadrez.cs
+++ XadrezConsole/xadrez/PartidaDeXadrez.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.AccessControl;
 using tabuleiro;
 using System.Collections.Generic;
 using XadrezConsole.xadrez;
@@ -39,6 +38,25 @@ namespace xadrez
             {
                 capturadas.Add(pecaCapturada);
             }
+
+            //#jogadaespecial roque pequeno
+            if(p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemT = new Posicao (origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQtdMovimentos();
+                tab.colocarPeca(T, destinoT);
+            }
+            //#jogadaespecial roque grande
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQtdMovimentos();
+                tab.colocarPeca(T, destinoT);
+            }
             return pecaCapturada;
 
         }
@@ -53,6 +71,26 @@ namespace xadrez
                 capturadas.Remove(pecaCapturada);
             }
             tab.colocarPeca(p, origem);
+
+            //#jogadaespecial roque pequeno
+            if (p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
+                Peca T = tab.retirarPeca(destinoT);
+                T.decrementarQtdMovimentos();
+                tab.colocarPeca(T, origemT);
+            }
+            //#jogadaespecial roque grande
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemT = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoT = new Posicao(origem.linha, origem.coluna - 1);
+                Peca T = tab.retirarPeca(origemT);
+                T.incrementarQtdMovimentos();
+                tab.colocarPeca(T, destinoT);
+            }
+            
         }
 
         public void realizaJogada(Posicao origem, Posicao destino)
@@ -64,6 +102,8 @@ namespace xadrez
                 desfazMovimento (origem, destino, pecaCapturada);
                 throw new TabuleiroException("Voce não pode se colocar em xeque");
             }
+
+            Peca p = tab.peca(destino);
 
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
@@ -232,7 +272,7 @@ namespace xadrez
             colocarNovaPeca('b', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('c', 1, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('d', 1, new Dama(tab, Cor.Branca));
-            colocarNovaPeca('e', 1, new Rei(tab, Cor.Branca));
+            colocarNovaPeca('e', 1, new Rei(tab, Cor.Branca, this));
             colocarNovaPeca('f', 1, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('g', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('h', 1, new Torre(tab, Cor.Branca));
@@ -249,7 +289,7 @@ namespace xadrez
             colocarNovaPeca('b', 8, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('c', 8, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('d', 8, new Dama(tab, Cor.Branca));
-            colocarNovaPeca('e', 8, new Rei(tab, Cor.Branca));
+            colocarNovaPeca('e', 8, new Rei(tab, Cor.Branca, this));
             colocarNovaPeca('f', 8, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('g', 8, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('h', 8, new Torre(tab, Cor.Branca));
--- XadrezConsole/xadrez/Peao.cs
+++ XadrezConsole/xadrez/Peao.cs
@@ -70,7 +70,7 @@ namespace xadrez
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
-                pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+                pos.definirValores(posicao.linha -1, posicao.coluna + 1);
                 if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
