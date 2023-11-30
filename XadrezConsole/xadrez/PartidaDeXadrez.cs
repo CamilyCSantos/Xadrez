62f788bb7e63d93c1dbfc1d9d374dff05c1f5f99
Camily Santos <camilycsantos@gmail.com>
2023-11-29 21:46:47 -0300

    Jogada especial en passant

--- XadrezConsole/xadrez/PartidaDeXadrez.cs
+++ XadrezConsole/xadrez/PartidaDeXadrez.cs
@@ -14,6 +14,7 @@ namespace xadrez
         private HashSet<Peca> pecas;
         private HashSet<Peca> capturadas;
         public bool xeque { get; private set; }
+        public Peca vulneravelEnPassant { get; private set; }
 
         public PartidaDeXadrez()
         {
@@ -22,6 +23,7 @@ namespace xadrez
             jogadorAtual = Cor.Branca;
             terminada = false;
             xeque = false;
+            vulneravelEnPassant = null;
             pecas = new HashSet<Peca>();
             capturadas = new HashSet<Peca>();
             colocarPecas();
@@ -57,6 +59,25 @@ namespace xadrez
                 T.incrementarQtdMovimentos();
                 tab.colocarPeca(T, destinoT);
             }
+
+            // #jogadaespecial en passant
+            if (p is Peao)
+            {
+                if (origem.coluna != destino.coluna && pecaCapturada == null)
+                {
+                    Posicao posP;
+                    if (p.cor == Cor.Branca)
+                    {
+                        posP = new Posicao(destino.linha + 1, destino.coluna);
+                    }
+                    else
+                    {
+                        posP = new Posicao(destino.linha - 1, destino.coluna);
+                    }
+                    pecaCapturada = tab.retirarPeca(posP);
+                    capturadas.Add(pecaCapturada);
+                }
+            }
             return pecaCapturada;
 
         }
@@ -90,7 +111,25 @@ namespace xadrez
                 T.incrementarQtdMovimentos();
                 tab.colocarPeca(T, destinoT);
             }
-            
+
+            // #jogadaespecial en passant
+            if (p is Peao)
+            {
+                if (origem.coluna != destino.coluna && pecaCapturada == vulneravelEnPassant)
+                {
+                    Peca peao = tab.retirarPeca(destino);
+                    Posicao posP;
+                    if (p.cor == Cor.Branca)
+                    {
+                        posP = new Posicao(3, destino.coluna);
+                    }
+                    else
+                    {
+                        posP = new Posicao(4, destino.coluna);
+                    }
+                    tab.colocarPeca(peao, posP);
+                }
+            }
         }
 
         public void realizaJogada(Posicao origem, Posicao destino)
@@ -103,8 +142,7 @@ namespace xadrez
                 throw new TabuleiroException("Voce não pode se colocar em xeque");
             }
 
-            Peca p = tab.peca(destino);
-
+            
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -122,6 +160,18 @@ namespace xadrez
             {
                 turno++;
                 mudaJogador();
+            }
+
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial en passant
+            if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
+            {
+                vulneravelEnPassant = p;
+            }
+            else
+            {
+                vulneravelEnPassant = null;
             }
         }
 
@@ -276,14 +326,14 @@ namespace xadrez
             colocarNovaPeca('f', 1, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('g', 1, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('h', 1, new Torre(tab, Cor.Branca));
-            colocarNovaPeca('a', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('b', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('c', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('d', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('e', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('f', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('g', 2, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('h', 2, new Peao(tab, Cor.Branca));
+            colocarNovaPeca('a', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('b', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('c', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('d', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('e', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('f', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('g', 2, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('h', 2, new Peao(tab, Cor.Branca, this));
 
             colocarNovaPeca('a', 8, new Torre(tab, Cor.Branca));
             colocarNovaPeca('b', 8, new Cavalo(tab, Cor.Branca));
@@ -293,14 +343,14 @@ namespace xadrez
             colocarNovaPeca('f', 8, new Bispo(tab, Cor.Branca));
             colocarNovaPeca('g', 8, new Cavalo(tab, Cor.Branca));
             colocarNovaPeca('h', 8, new Torre(tab, Cor.Branca));
-            colocarNovaPeca('a', 7, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('b', 7, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('c', 7, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('d', 7, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('e', 7, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('f', 7, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('g', 7, new Peao(tab, Cor.Branca));
-            colocarNovaPeca('h', 7, new Peao(tab, Cor.Branca));
+            colocarNovaPeca('a', 7, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('b', 7, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('c', 7, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('d', 7, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('e', 7, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('f', 7, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('g', 7, new Peao(tab, Cor.Branca, this));
+            colocarNovaPeca('h', 7, new Peao(tab, Cor.Branca, this));
 
 
         }
